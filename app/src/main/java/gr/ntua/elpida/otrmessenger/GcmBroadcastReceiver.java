4038eaa06fe9ef00c6eebc981ce561a3d98d5426
elpidaro <rouka.elpida@gmail.com>
2016-12-08 19:01:46 +0200

    permissions and common application

---
+++ app/src/main/java/gr/ntua/elpida/otrmessenger/GcmBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package gr.ntua.elpida.otrmessenger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by elpida on 12/8/16.
+ */
+
+public class GcmBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
